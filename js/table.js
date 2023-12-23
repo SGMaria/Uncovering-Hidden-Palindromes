c2797ffafc9704cc5452950137df95cc69ed1731
Maria Luque <33879614+SGMaria@users.noreply.github.com>
2023-12-23 10:50:06 -0300

    Working on table visuals

--- js/table.js
+++ js/table.js
@@ -6,12 +6,17 @@ function init(){
 
 function table_list(){
     let table = document.getElementById("results");
+    console.log(table);
+    table = table.getElementsByTagName("tbody")[0];
+    console.log(table);
     let list = show_saved();
-    let row = table.insertRow(1);
+    let row = table.insertRow(0);
     let cell1 = row.insertCell(0);
-    let cell2 = row.insertCell(0);
-    cell1.innerHTML = list;
-    cell2.innerHTML = document.getElementById("input_for_string").value;
+    let cell2 = row.insertCell(1);
+    let cell3 = row.insertCell(2);
+    cell1.innerHTML = document.getElementById("input_for_string").value;
+    cell2.innerHTML = list.length;
+    cell3.innerHTML = list;
 }
 
 document.getElementById("start").addEventListener("click", init);
